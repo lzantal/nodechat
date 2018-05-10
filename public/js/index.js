f8c61676320d710c031c729c937a119d2d09de5f
LZAntal <LZAntal@gmail.com>
2018-05-09 21:39:38 -0700

    styled the chat page with bootstrap 4, no mobile support for now

--- public/js/index.js
+++ public/js/index.js
@@ -12,15 +12,16 @@ socket.on('newMessage', (msg) => {
     console.log('New message');
     //console.table(msg);
     $('#allmsg').append(`<li>${msg.from}: ${msg.text}</li>`);
+    $('#msg-panel')[0].scrollTo(0, 100000);
 });
 
 // handle html and form to send message
-$('#msg-form').on('submit', (e) => {
+$('#sendbtn').on('click', (e) => {
     e.preventDefault();
     socket.emit('createMessage', {
         from: 'user',
         text: $('#msg').val()
     }, (retval) => {
-        $('#msg').val('');
+        $('#msg').val('').focus();
     });
 });
